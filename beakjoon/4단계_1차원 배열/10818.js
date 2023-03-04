7fbea1ddd030a853aa1b7df9bf74d7b3dc51a6c4
jihwanAn <an0572@naver.com>
2023-03-04 17:42:09 +0900

    [BOJ] 최소, 최대 / 브론즈 3

---
+++ beakjoon/4단계_1차원 배열/10818.js
@@ -0,0 +1,25 @@
+let fs = require("fs");
+let input = fs.readFileSync("10818.txt").toString().trim().split("\n");
+
+// let count = input[0];
+// let nums = input[1].split(" ").map(Number);
+
+// let max = nums[0];
+// let min = nums[0];
+
+// for (let i = 0; i < count; i++) {
+//   if (max < nums[i]) {
+//     max = nums[i];
+//   }
+//   if (min > nums[i]) {
+//     min = nums[i];
+//   }
+// }
+// console.log(min, max);
+
+let arr = input[1]
+  .split(" ")
+  .map((v) => Number(v))
+  .sort((a, b) => a - b);
+
+console.log(`${arr[0]} ${arr[arr.length - 1]}`);

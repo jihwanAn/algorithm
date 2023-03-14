6db2b7d30a0780ad3ad75d7476fc35b634d3ee69
jihwanAn <an0572@naver.com>
2023-03-14 23:50:50 +0900

    [BOJ] 공 바꾸기 / 브론즈 2

---
+++ beakjoon/4단계_1차원 배열/10813.js
@@ -0,0 +1,20 @@
+let input = require("fs")
+  .readFileSync("10813.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+let [N, M] = input[0].split(" ").map(Number);
+
+let arr = Array(N)
+  .fill(1)
+  .map((a, b) => a + b);
+
+for (let i = 1; i <= M; i++) {
+  let [x, y] = input[i].split(" ").map(Number);
+  let tmp = arr[x - 1];
+  arr[x - 1] = arr[y - 1];
+  arr[y - 1] = tmp;
+}
+
+console.log(arr.join(" "));

1059f730f0de571a9e84bc80702314eac291c463
jihwanAn <an0572@naver.com>
2023-03-21 15:53:07 +0900

    [BOJ] 평균은 넘겠지 / 브론즈 1

--- beakjoon/6단계_심화/4344.js
+++ beakjoon/6단계_심화/4344.js
@@ -1,38 +1,16 @@
 let input = require("fs").readFileSync("4344.txt").toString().split("\n");
 
-let tc = Number(input[0]);
+let total = Number(input[0]);
 
-for (let i = 1; i <= tc; i++) {
-  let [stuNums, ...scoreArr] = input[i].split(" ").map(Number);
+for (let i = 1; i <= total; i++) {
+  let [nums, ...scoreArr] = input[i].split(" ").map(Number);
+  // 평균 구하기
+  let avg = scoreArr.reduce((a, c) => a + c) / nums;
 
-  let avgOfStuArr = scoreArr.reduce((acc, cur) => acc + cur) / stuNums;
-  let filterStuAvg = scoreArr.filter((ele) => {
-    return ele > avgOfStuArr;
+  // 평균을 넘는 학생
+  let filterAvg = scoreArr.filter((e) => {
+    return e > avg;
   });
 
-  console.log(((filterStuAvg.length / stuNums) * 100).toFixed(3) + "%");
+  console.log(((filterAvg.length / nums) * 100).toFixed(3) + "%");
 }
-
-// let num = Number(input[0]);
-
-// for (let i = 1; i <= num; i++) {
-//   let sum = 0;
-//   let count = 0;
-
-//   scores = input[i].split(" ").map(Number);
-
-//   const n = scores[0];
-
-//   for (let j = 1; j <= n; j++) {
-//     sum += scores[j];
-//   }
-
-//   const avg = sum / n;
-
-//   for (let k = 1; k <= n; k++) {
-//     if (scores[k] > avg) {
-//       count++;
-//     }
-//   }
-//   console.log(((count / n) * 100).toFixed(3) + "%");
-// }

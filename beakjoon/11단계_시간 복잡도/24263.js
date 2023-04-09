a695b0420ba3a19c07a37d9923638690397481fa
jihwanAn <an0572@naver.com>
2023-04-09 23:58:18 +0900

    [BOJ] 알고리즘 수업 - 알고리즘의 수행 시간 2 / 브론즈 4

---
+++ beakjoon/11단계_시간 복잡도/24263.js
@@ -0,0 +1,11 @@
+// MenOfPassion(A[], n) {
+//   sum <- 0;
+//   for i <- 1 to n
+//       sum <- sum + A[i]; # 코드1
+//   return sum;
+// }
+
+let input = require("fs").readFileSync("24263.txt").toString();
+
+console.log(input);
+console.log(1);

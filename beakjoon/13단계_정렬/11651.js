fd7e6aee2bd0bd869d35310df602e6fff4148372
jihwanAn <an0572@naver.com>
2023-04-21 15:26:53 +0900

    [BOJ] 좌표 정렬하기 2 / 실버 5

---
+++ beakjoon/13단계_정렬/11651.js
@@ -0,0 +1,22 @@
+let input = require("fs").readFileSync("11650.txt").toString().split("\n");
+
+const N = Number(input.shift());
+
+let arr = [];
+let result = [];
+
+for (let i = 0; i < N; i++) {
+  arr.push(input[i].split(" ").map((v) => Number(v)));
+}
+
+arr.sort((a, b) => {
+  if (a[1] === b[1]) return a[0] - b[0];
+  else return a[1] - b[1];
+});
+
+for (let i = 0; i < N; i++) {
+  let num = arr[i].join(" ");
+  result.push(num);
+}
+
+console.log(result.join("\n"));

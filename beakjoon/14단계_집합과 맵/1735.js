387afad593332a3d79f37364963495fa949a040f
jihwanAn <an0572@naver.com>
2023-04-30 19:07:14 +0900

    [BOJ] 분수 합 / 실버 3

---
+++ beakjoon/14단계_집합과 맵/1735.js
@@ -0,0 +1,19 @@
+const input = require("fs")
+  .readFileSync("1735.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const gcd = (a, b) => {
+  if (a % b === 0) return b;
+  return gcd(b, a % b);
+};
+
+let [a, b] = input[0].split(" ").map(Number);
+let [c, d] = input[1].split(" ").map(Number);
+
+let numerator = a * d + b * c;
+let denominator = b * d;
+let e = gcd(numerator, denominator);
+
+console.log(numerator / e, denominator / e);

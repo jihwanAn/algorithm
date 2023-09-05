97a9d1306afb2ba1ca0de3e29fd98aaf83b84f1c
jihwanAn <an0572@naver.com>
2023-09-05 21:26:19 +0900

    [BOJ] 중량제한 / 골드 3

---
+++ beakjoon/27단계_이분 탐색/1939.js
@@ -0,0 +1,67 @@
+function BFS(n, city, start, end, mid) {
+  let visited = new Array(n + 1).fill(false);
+  let q = [];
+  q.push(start);
+
+  while (q.length !== 0) {
+    let cur = q.shift();
+    visited[cur] = true;
+
+    if (cur === end)
+      // 도착지 까지 도착.
+      return true;
+
+    for (let i = 0; i < city[cur].length; i++) {
+      let next = city[cur][i][0]; // 다음 도시.
+      let nextCost = city[cur][i][1];
+
+      if (!visited[next] && mid <= nextCost) {
+        visited[next] = true;
+        q.push(next);
+      }
+    }
+  }
+  return false;
+}
+
+function binarySearch(n, city, start, end, max) {
+  let left = 1;
+  let right = max;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (BFS(n, city, start, end, mid))
+      // 통과가능한 중량이면 중량++
+      left = mid++;
+    else {
+      right = mid--;
+    }
+  }
+  console.log(right);
+}
+
+function insert() {
+  const input = require("fs")
+    .readFileSync("1939.txt")
+    .toString()
+    .trim()
+    .split("\n");
+  const [n, m] = input[0].split(" ").map(Number);
+
+  let city = new Array(n + 1).fill(null).map((_) => []);
+  let max = 0;
+
+  for (let i = 1; i <= m; i++) {
+    let [from, to, weigth] = input[i].split(" ").map(Number);
+
+    if (weigth > max) max = weigth;
+    city[from].push([to, weigth]);
+    city[to].push([from, weigth]);
+  }
+  let [start, end] = input[m + 1].split(" ").map(Number);
+
+  binarySearch(n, city, start, end, max);
+}
+
+insert();
